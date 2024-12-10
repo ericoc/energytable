a5f9015de2455f21e18e4d4f00ec1c2e18ddb497
Eric O'Callaghan <eric@ericoc.com>
2024-12-10 00:01:22 -0500

    filled.

--- usage/management/commands/fill.py
+++ usage/management/commands/fill.py
@@ -1,7 +1,8 @@
 from csv import DictReader
-from datetime import date
-from django.conf import settings
+from datetime import date, datetime, time
 from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+from pathlib import Path
 from ...models import EnergyUsage
 
 
@@ -14,41 +15,66 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        num_created = 0
+        # peco_electric_usage_interval_data_Service 1_1_2023-01-21_to_2023-12-07.csv
+        csv_prefix = "peco_electric_usage_interval_data_Service 1_1_"
+        csv_files = sorted(
+            list(Path(".").glob(f"{csv_prefix}*.csv")),
+            reverse=True
+        )
         try:
-            # Open comma-separated values ("CSV") file of energy usage data.
-            with open(file=settings.IMPORT_FILENAME, mode="r") as csv_fh:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"{len(csv_files)} CSV file(s) found."
+                )
+            )
+            num_created = 0
 
-                # Iterate each row of the CSV file.
-                for csv_row in DictReader(csv_fh):
+            for csv_file in csv_files:
+                csv_path = Path(csv_file)
+                csv_name = csv_path.name.replace(csv_prefix, "")
+                csv_stat = csv_path.stat()
+                csv_size = csv_stat.st_size
 
-                    # Ensure the row is measuring gallons (not CCFs, etc.)
-                    assert csv_row[" Units"] == " Gallons", "Invalid unit."
+                if csv_size == 241:
+                    self.stdout.write(self.style.NOTICE(f"Skipped: {csv_name}"))
+                    continue
 
-                    # Parse the date column within each CSV row.
-                    date_parts = csv_row[" Time Interval"].strip().split('/')
-                    date_iso = \
-                        f"{date_parts[2]}-{date_parts[0]}-{date_parts[1]}"
+                with csv_path.open(mode="r", encoding="utf-8-sig") as read_fh:
+                    csv_lines = read_fh.readlines()
+                    for row in DictReader(csv_lines[6:]):
+                        assert row["TYPE"] == "Electric usage", "Invalid type!"
 
-                    # Create dictionary mapping hour to energy usage in gallons.
-                    usage = {
-                        "date": date.fromisoformat(date_iso),
-                        "gallons": float(csv_row[" Consumption"].strip())
-                    }
+                        # Parse the date and time columns within each CSV row.
+                        time_parts = row["START TIME"].split(':')
 
-                    # Update or create the energy usage object in the database.
-                    obj, created = EnergyUsage.objects.update_or_create(
-                        **usage,
-                        defaults=usage
-                    )
+                        # Map datetime of hour to electricity usage (in kWh).
+                        usage = {
+                            "hour": timezone.make_aware(
+                                value=datetime.combine(
+                                    date=date.fromisoformat(row["DATE"]),
+                                    time=time(
+                                        hour=int(time_parts[0]),
+                                        minute=int(time_parts[1])
+                                    )
+                                )
+                            ),
+                            "kwh": float(row["USAGE (kWh)"])
+                        }
 
-                    # Show any newly created energy usage database objects.
-                    if created:
-                        num_created += 1
-                        self.stdout.write(self.style.SUCCESS(
-                            f"Created:\t{obj.date.strftime("%A, %B %d, %Y")}"
-                            f" ({obj.date}) [{obj.gallons} gallons]"
-                        ))
+                        # Update or create energy usage object in the database.
+                        obj, created = EnergyUsage.objects.update_or_create(
+                            **usage,
+                            defaults=usage
+                        )
+
+                        # Show any newly created energy usage database objects.
+                        if created:
+                            num_created += 1
+                            self.stdout.write(self.style.SUCCESS(
+                                "Created:\t"
+                                f"{obj.hour.strftime("%A, %B %d, %Y @ %I %p")}"
+                                f" ({obj.hour}) [{obj.kwh} kWh]"
+                            ))
 
             # List total count of newly created energy usage database objects.
             if num_created > 0:
