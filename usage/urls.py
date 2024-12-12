e5bf7732dbdfb8611eb5810a7532e9675e5cbf8c
Eric O'Callaghan <eric@ericoc.com>
2024-12-11 20:43:57 -0500

    energyyyy

--- usage/urls.py
+++ usage/urls.py
@@ -1,13 +1,12 @@
 from django.urls import include, path
 
 from .api import api_router
-from .views import EnergyChartView
+from .views import EnergyChartView, EnergyTableView
 
 
 urlpatterns = [
     path("api/", include(api_router.urls), name="api"),
     path("", EnergyChartView.as_view(), name="chart"),
-#    path("chart/", EnergyChartView.as_view(), name="chart"),
-#    path("table/", EnergyTableView.as_view(), name="table"),
+    path("table/", EnergyTableView.as_view(), name="table"),
 
 ]
--- usage/views.py
+++ usage/views.py
@@ -15,15 +15,6 @@ class EnergyUsageBaseView(TemplateView):
         return context
 
 
-class EnergyIndexView(ListView, EnergyUsageBaseView):
-    """Energy usage index view."""
-    context_object_name = "items"
-    model = EnergyUsage
-    queryset = model.objects.all()
-    template_name = "index.html"
-    title = "Index"
-
-
 class EnergyTableView(EnergyUsageBaseView):
     """Energy usage table view."""
     template_name = "table.html"
